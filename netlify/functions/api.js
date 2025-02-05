26163674d6a9b48c4a52ce7f54ab571f8bd3bdb2
shubham94 <shubhamchinchole@gmail.com>
2025-02-06 00:22:26 +0530

    update code for deploy on netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { Router } from "express";
 
 import "dotenv/config";
 
+import ServerlessHttp from "serverless-http";
+
+const router = Router();
 const app = express();
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -12,7 +15,7 @@ let teaData = [];
 let nextId = 1;
 
 // add a new tea
-app.post("/teas", (req, res) => {
+router.post("/teas", (req, res) => {
   const { name, price } = req.body;
 
   const newTea = { id: nextId++, name, price };
@@ -21,12 +24,12 @@ app.post("/teas", (req, res) => {
 });
 
 //get all teas
-app.get("/teas", (req, res) => {
+router.get("/teas", (req, res) => {
   res.status(200).send(teaData);
 });
 
 // get a tea with id
-app.get("/teas/:id", (req, res) => {
+router.get("/teas/:id", (req, res) => {
   const tea = teaData.find((t) => t.id === parseInt(req.params.id));
 
   if (!tea) {
@@ -38,7 +41,7 @@ app.get("/teas/:id", (req, res) => {
 
 //update tea
 
-app.put("/teas/:id", (req, res) => {
+router.put("/teas/:id", (req, res) => {
   const tea = teaData.find((t) => t.id === parseInt(req.params.id));
 
   if (!tea) {
@@ -53,7 +56,7 @@ app.put("/teas/:id", (req, res) => {
 
 //delete tea
 
-app.delete("/teas/:id", (req, res) => {
+router.delete("/teas/:id", (req, res) => {
   const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
 
   if (index === -1) {
@@ -63,6 +66,9 @@ app.delete("/teas/:id", (req, res) => {
   return res.status(200).send("Tea Deleted ");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at port: ${port}...`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running at port: ${port}...`);
+// });
+
+app.use("/api/", router);
+export const handler = ServerlessHttp(app);
